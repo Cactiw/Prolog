fa2ac8e70342c6600bee11d6d714b2d8547ef1b0
Cactiw <Knyazev1514@gmail.com>
2020-12-09 23:05:22 +0300

    Added main menu

--- ontology/ontology.pl
+++ ontology/ontology.pl
@@ -3,7 +3,36 @@
 :- [knowledge].
 
 
+main() :-
+    writeln("Здравствуйте!\n1) Отобразить все классы\n2) Отобразить все свойства класса\n3) Найти различия двух классов\n4) Добавить класс\n5) Добавить свойство к классу\n\nq) Выход"),
+    readln([Action | _]),
+    menuAction(Action),
+    writeln("\n\n"),
+    main().
+
+
+menuAction(1) :-
+    writeln("Not implemented").
+
+menuAction(2) :-
+    display_properties().
+
+menuAction(3) :-
+    display_differences().
+
+menuAction(4) :-
+    add_class().
+
+menuAction(5) :-
+    add_property().
+
+menuAction(q) :-
+    exit().
 
 exit() :-
     saveDB(),
     halt.
+
+
+:- initialization(main, main).
+
