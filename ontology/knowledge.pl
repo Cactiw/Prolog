165b32af0060298634da3789b6dc7d64ab47ef76
Cactiw <Knyazev1514@gmail.com>
2020-12-10 13:29:48 +0300

    Expanded the knowledge base

--- ontology/knowledge.pl
+++ ontology/knowledge.pl
@@ -23,6 +23,20 @@ inherit(bus, minibus).
 
 inherit(surface, trolley).
 inherit(surface, taxi).
+inherit(surface, train).
+
+inherit(trane, local_train).
+inherit(trane, express_train).
+inherit(trane, locomotive).
+inherit(train, speed_train).
+
+inherit(air, plane).
+inherit(air, helicopter).
+
+inherit(water, boat).
+inherit(water, ship).
+
+inherit(underground, subway).
 
 
 property(surface, shedule, random_shedule).
@@ -30,18 +44,54 @@ property(air, shedule, fixed_shedule).
 property(water, shedule, fixed_shedule).
 property(underground, shedule, random_shedule).
 
+property(surface, capacity, many).
+property(air, capacity, many).
+property(water, capacity, small).
+property(underground, capacity, many).
 
+property(surface, comfort, good).
+property(air, comfort, bad).
+property(water, comfort, bad).
+property(underground, comfort, good).
+
+property(surface, places, random).
+property(air, places, fixed).
+property(water, places, fixed).
+property(underground, places, random).
+
+
+property(locomotive, engine, coal).
+property(train, engine, electro).
+
+
+property(water, engine, fuel).
+property(air, engine, fuel).
+
+property(underground, engine, electro).
+
+property(boat, engine, electro).
+property(boat, places, random).
+
+property(ship, capacity, many).
 
 property(bus, engine, fuel).
 property(bus, shedule, fixed_shedule).
 
 property(minibus, engine, fuel).
+property(minibus, capacity, small).
+property(minibus, comfort, bad).
 
 property(electrobus, engine, electro).
 
 property(trolley, engine, electro).
 
 property(taxi, engine, fuel).
+property(taxi, places, fixed).
+
+property(helicopter, capacity, small).
+property(helicopter, places, random).
+
+property(plain, comfort, good).
 
 
 saveDB() :-
