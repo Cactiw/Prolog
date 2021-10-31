7ac62760fe7bb625dcfa96960f8725bc01a6feaa
Cactiw <Knyazev1514@gmail.com>
2021-10-31 22:52:15 +0300

    Added other functions

--- Maga/HT_Maze/maze.pl
+++ Maga/HT_Maze/maze.pl
@@ -7,7 +7,7 @@ connected(X, Y, [[A, B] | Maze]) :-
 find_path(Maze, Path) :-
     find_path(in, out, Maze, Path).
 find_path(In, Out, Maze, Path) :-
-    path(In, Out, Maze, [in], Path).
+    path(In, Out, Maze, [In], Path).
 
 elem_in_list(E, [E|L]).
 elem_in_list(E, [X|T]):-
@@ -20,3 +20,51 @@ path(Current, Out, Maze, Visited, [Current | Path]) :-
     connected(Current, NewRoom, Maze),
     not(elem_in_list(NewRoom, Visited)),
     path(NewRoom, Out, Maze, [NewRoom | Visited], Path).
+
+
+% (i, o), [(i, i) - только в правильном порядке] 
+% Создаёт множество из списка (удаляет повторяющиеся элементы)
+create_set([], []).
+create_set([X|T], [X|S]) :-
+    delete_all(X, T, NewT),
+    create_set(NewT, S).
+
+% 1 - любые прототипы, неопределён только (o, o, o)
+my_append([], L, L).
+my_append([X|L1], L2, [X|L3]):- my_append(L1, L2, L3).
+
+
+% 4 - (i, i, o), (i, i, i), (o, i, i), (i, o, i)
+delete_all(_, [], []).
+delete_all(E, [E|T], Res) :-
+    !, delete_all(E, T, Res).
+delete_all(E, [X|T], [X|Res]) :-
+    delete_all(E, T, Res).
+
+find_all_rooms_from_ends(Maze, ResultNoDublicates) :-
+    findall(Room, find_path(in, Room, Maze, _), ResIn),
+    findall(Room, find_path(out, Room, Maze, _), ResOut),
+    my_append(ResIn, ResOut, Result),
+    create_set(Result, ResultNoDublicates).
+
+% room(Maze, Room).
+room([[Room, Room2] | Maze], Room).
+room([[Room, Room2] | Maze], Room2).
+room([Path | Maze], Room) :-
+    room(Maze, Room).
+
+% all_rooms(Maze, Rooms) :-
+%     findall(NewRoom, room(Maze, NewRoom), RoomsWithDublicates),
+%     create_set(RoomsWithDublicates, Rooms).
+
+closed_room(Maze, Room) :-
+    find_all_rooms_from_ends(Maze, RoomsWithPath),
+    room(Maze, Room),
+    not(member(Room, RoomsWithPath)).
+
+find_all_closed_rooms(Maze, Result):-
+    findall(ClosedRoom, closed_room(Maze, ClosedRoom), ResultWithDublicates),
+    create_set(ResultWithDublicates, Result).
+
+
+
